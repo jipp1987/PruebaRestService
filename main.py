543eac523dccceda865672a05bffc4442de10ade
jipp1987 <jipp1987@gmail.com>
2020-10-25 08:52:11 +0100

    Implementado rest service usando flask. Aún no se ha probado.

--- main.py
+++ main.py
@@ -1,25 +1,37 @@
-# This is a sample Python script.
+from flask import Flask, request
 
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-# Press the green button in the gutter to run the script.
-from core.service.service import ServiceFactory
-from core.util import i18n
+# Defino la app de Flask a partir del nombre del este módulo. Es la representación de la aplicación web.
 from core.exception.exceptionhandler import CustomException
+from core.service.service import ServiceFactory
 from model.tipocliente import TipoCliente
 from service.tipoclienteservice import TipoClienteService
 
-if __name__ == '__main__':
-    i18n.change_locale("es_ES")
+app = Flask(__name__)
 
-    tipocliente = TipoCliente(None, "0005", "Contado")
-    tipoclienteService = ServiceFactory.get_service(TipoClienteService)
+
+# Esto es la ruta del servicio web dentro del servidor en que se haya desplegado
+@app.route("/insert_tipo_cliente", methods=['POST'])
+def insert_tipo_cliente():
+    """
+    Rest service para insertar nuevos tipos de cliente.
+    :return: Cadena con mensaje formateado para devolver al solicitante.
+    """
+    # Obtengo datos json de la petición
+    req_data = request.get_json()
+    codigo = req_data['codigo']
+    descripcion = req_data['descripcion']
 
     try:
-        tipoclienteService.start_transaction(tipoclienteService.insert, tipocliente)
-        print(f'Se ha insertado {str(tipocliente)}')
+        # Inserto el tipo de cliente
+        tipo_cliente = TipoCliente(None, codigo, descripcion)
+        tipo_cliente_service = ServiceFactory.get_service(TipoClienteService)
+        tipo_cliente_service.insert(tipo_cliente)
+        return '''<h1>Se ha insertado: {}</h1>'''.format(str(tipo_cliente))
     except CustomException as e:
-        print(str(e))
+        return f'Se ha producido un error insertando el registro: {str(e)}'
+
+
+if __name__ == "__main__":
+    # Con esta línea arranco la aplicación, y le activo el modo debug para visualizar errores en la propia web
+    app.run(debug=True, port=5000)
 
